ab01f00bf21d9ae9b89adc4e5f86d058199e4642
ASUS <ASUS@DESKTOP-DJ6E0IE>
2026-06-15 06:18:30 +0700

    membuat input password jadi tanda *

--- login/FileName.cpp
+++ login/FileName.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <iomanip>
 #include <Windows.h>
+#include <conio.h>
 
 using namespace std;
 
@@ -20,6 +21,7 @@ int jumlahKurir = 0;
 
 void Registrasi();
 void Login();
+string inputPasswordTersembunyi();
 
 string spasi = "                    ";
 
@@ -64,6 +66,29 @@ int main() {
 
 }
 
+string inputPasswordTersembunyi() {
+    string password = "";
+    char ch;
+    while (true) {
+        ch = _getch(); 
+        if (ch == '\r') { 
+            break;
+        }
+        else if (ch == '\b') { 
+            if (!password.empty()) {
+                password.pop_back();
+                cout << "\b \b"; 
+            }
+        }
+        else if (ch >= 32 && ch <= 126) {
+            password.push_back(ch);
+            cout << '*';
+        }
+    }
+    cout << endl;
+    return password;
+}
+
 void Registrasi() {
 	bool regis = true;
     do {
@@ -86,7 +111,7 @@ void Registrasi() {
         cin >> user;
 
         cout << spasi << "| Password: ";
-        cin >> pass;
+        pass = inputPasswordTersembunyi();
 
         switch (role) {
         case 1:
@@ -162,7 +187,7 @@ void Login() {
         cin >> user;
 
         cout << spasi << "| Password: ";
-        cin >> pass;
+        pass = inputPasswordTersembunyi();
 
         switch (role) {
         case 1:
